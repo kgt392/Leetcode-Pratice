38584457a20d87b68911e7d5fd2cfa7793591742
kgt392 <krishnagtyagi1@gmail.com>
2026-01-30 19:36:36 +0530

    Time: 12 ms (32.39%), Space: 7.8 MB (36.16%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return fib(n-1)+fib(n-2);
+        if(n==1 || n==2){
+            return 1;
+        }
+        else if (n==0){
+            return 0;
+        }
+        else {
+            return fib(n-1)+fib(n-2);
+        }
     }
 };
