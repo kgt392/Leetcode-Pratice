b7ed0c4ef2ed6deaea013a5ffd425af04bd62068
kgt392 <krishnagtyagi1@gmail.com>
2026-03-15 22:44:23 +0530

    Time: 95 ms (5.12%), Space: 23.9 MB (55.2%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        int temp[n];
-        int k = 0;
-        for(int i=0;i<n;i++){
-            if(nums[i]!=0){
-                temp[k] = nums[i];
-                k++;
-            }
-        }
-       while(k < n){
-            temp[k] = 0;
-            k++;
-        }
+    void moveZeroes(vector<int>& arr) {
+        int n = arr.size();
+    //     int temp[n];
+    //     int k = 0;
+    //     for(int i=0;i<n;i++){
+    //         if(nums[i]!=0){
+    //             temp[k] = nums[i];
+    //             k++;
+    //         }
+    //     }
+    //    while(k < n){
+    //         temp[k] = 0;
+    //         k++;
+    //     }
     
-        for(int i = 0; i < n; i++){
-            nums[i] = temp[i];
-            cout<<nums[i];
+    //     for(int i = 0; i < n; i++){
+    //         nums[i] = temp[i];
+    //         cout<<nums[i];
+    //     }
+
+    int j = -1;
+    for(int i =0;i<n;i++){
+        if(arr[i] ==0){
+            j=i;
+            break;
+        }
+    }
+
+    if (j == -1) return ;
+
+    for(int i =j+1;i<n;i++){
+        if(arr[i] !=0){
+            swap(arr[i],arr[j]);
+            j++;
+        }
+    }
+     for(int i = 0; i < n; i++){
+            cout<<arr[i];
         }
     }
 };
