0d1244e2e67724ae451d31bcebaee3c81a600309
kgt392 <krishnagtyagi1@gmail.com>
2026-03-28 09:30:56 +0530

    Time: 2314 ms (5.4%), Space: 23.7 MB (64.35%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,14 +3,29 @@ public:
     vector<int> majorityElement(vector<int>& arr) {
         vector<int> ls;
         int n =arr.size();
-        int min =(int)(n/3)+1;
-        map <int,int>mpp;
+        // int min =(int)(n/3)+1;
+        // map <int,int>mpp;
+        // for(int i =0;i<n;i++){
+        //     mpp[arr[i]]++;
+        //     if(mpp[arr[i]] == min){
+        //         ls.push_back(arr[i]);
+        //     }  
+        //     if(ls.size()==2) break;
+        // }
+        // return ls;
         for(int i =0;i<n;i++){
-            mpp[arr[i]]++;
-            if(mpp[arr[i]] == min){
-                ls.push_back(arr[i]);
-            }  
-            if(ls.size()==2) break;
+            if(ls.size()==0 || ls[0] != arr[i]){
+                int cnt=0;
+                for(int j=0;j<n;j++){
+                    if(arr[j] == arr[i]){
+                        cnt++;
+                    }
+                }
+                if(cnt> n/3){
+                    ls.push_back(arr[i]);
+                }
+            }
+             if (ls.size()==2 ) break;
         }
         return ls;
     }
