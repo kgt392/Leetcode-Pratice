f1290c3846649d6dfd1a748a18ff6cbbbb6795ab
kgt392 <krishnagtyagi1@gmail.com>
2026-03-28 09:31:24 +0530

    Time: 15 ms (9.66%), Space: 26.8 MB (11.05%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,30 +3,32 @@ public:
     vector<int> majorityElement(vector<int>& arr) {
         vector<int> ls;
         int n =arr.size();
-        // int min =(int)(n/3)+1;
-        // map <int,int>mpp;
-        // for(int i =0;i<n;i++){
-        //     mpp[arr[i]]++;
-        //     if(mpp[arr[i]] == min){
-        //         ls.push_back(arr[i]);
-        //     }  
-        //     if(ls.size()==2) break;
-        // }
-        // return ls;
+        int min =(int)(n/3)+1;
+        map <int,int>mpp;
         for(int i =0;i<n;i++){
-            if(ls.size()==0 || ls[0] != arr[i]){
-                int cnt=0;
-                for(int j=0;j<n;j++){
-                    if(arr[j] == arr[i]){
-                        cnt++;
-                    }
-                }
-                if(cnt> n/3){
-                    ls.push_back(arr[i]);
-                }
-            }
-             if (ls.size()==2 ) break;
+            mpp[arr[i]]++;
+            if(mpp[arr[i]] == min){
+                ls.push_back(arr[i]);
+            }  
+            if(ls.size()==2) break;
         }
         return ls;
+
+        // ==========================
+        // for(int i =0;i<n;i++){
+        //     if(ls.size()==0 || ls[0] != arr[i]){
+        //         int cnt=0;
+        //         for(int j=0;j<n;j++){
+        //             if(arr[j] == arr[i]){
+        //                 cnt++;
+        //             }
+        //         }
+        //         if(cnt> n/3){
+        //             ls.push_back(arr[i]);
+        //         }
+        //     }
+        //      if (ls.size()==2 ) break;
+        // }
+        // return ls;
     }
 };
